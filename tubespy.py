c3f373c5ab749488ad1faac7dcd8a97266d2fb2e
I Kadek Rizky Dwitama Widiasa <72237047+rizkydwitama@users.noreply.github.com>
2022-06-15 12:29:07 +0800

    REVISI PERTAMA

--- tubespy.py
+++ tubespy.py
@@ -6,18 +6,46 @@ from mininet.log import setLogLevel
 import time
 import os
 
-#BUILD TOPOLOGY
-HostA = net.addHost('HostA')
-HostB = net.addHost('HostB')
-R1 = net.addHost('R1')
-R2 = net.addHost('R2')
-R3 = net.addHost('R3')
-R4 = net.addHost('R4')
+if '__main__' == __name__:
+	os.system('mn -c')
+	setLogLevel('info')
+	net = Mininet(link=TCLink)
+	key = "net.mptcp.mptcp_enabled"
+	value = 0
+	p = Popen("sysctl -w %s=%s" %(key,value), shell=True, stdout=PIPE, stderr=PIPE)
+	stdout, stderr = p.communicate()
+	print("stdout=",stdout,"stderr=",stderr)
 
-#MENDEFINISIKAN BANDWIDTH
-bandwidth1 = {'bw':1}
-bandwidth2 = {'bw':0.5}
+    #BUILD TOPOLOGY
+    #ADD HOST
+    HostA = net.addHost('HostA')
+    HostB = net.addHost('HostB')
 
-#MENGHUBUNGKAN ANTAR DEVICE
-net.addLink(HostA, R1, cls=TCLink, **bandwidth1) #HostA-eth0 R1-eth0
-net.addLink(HostA, R2, cls=TCLink, **bandwidth1) #HostA-eth1 R3-eth1
+    #ADD ROUTER
+    R1 = net.addHost('R1')
+    R2 = net.addHost('R2')
+    R3 = net.addHost('R3')
+    R4 = net.addHost('R4')
+
+    #MENDEFINISIKAN BANDWIDTH
+    bwA = {'bw':1}
+    bwB = {'bw':0.5}
+
+    
+    #MENGHUBUNGKAN ANTAR DEVICE
+    #Add link 
+	net.addLink(HostA, R1, intfName1='HostA-eth0', intfName2='R1-eth0', cls=TCLink, **bwA)
+    net.addLink(HostA, R2, intfName1='HostA-eth1', intfName2='R2-eth1', cls=TCLink, **bwA)
+
+    net.addLink(R1, R3, intfName1='R1-eth1', intfName2='R3-eth1', cls=TCLink, **bwB)
+    net.addLink(R1, R4, intfName1='R1-eth2', intfName2='R4-eth2', cls=TCLink, **bwA)
+
+    net.addLink(R3, HostB, intfName1='R3-eth0', intfName2='HostB-eth0', cls=TCLink, **bwA)
+    net.addLink(R3, R2, intfName1='R3-eth2', intfName2='R2-eth2', cls=TCLink, **bwA)
+
+    net.addLink(HostB, R4, intfName1='HostB-eth1', intfName2='R4-eth1', cls=TCLink, **bwA)
+    net.addLink(R4, R2, intfName1='R4-eth0', intfName2='R2-eth0', cls=TCLink, **bwB)
+
+    net.build()
+    net.start
+    CLI(net)
